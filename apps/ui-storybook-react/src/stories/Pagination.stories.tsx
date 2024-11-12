bfd09eb41f0581bd7573c4efcbff57433c30103d
Hamza Rafique <hamzarafique964@gmail.com>
2024-11-12 21:34:43 +0300

    pagination ui updated

--- apps/ui-storybook-react/src/stories/Pagination.stories.tsx
+++ apps/ui-storybook-react/src/stories/Pagination.stories.tsx
@@ -18,6 +18,55 @@ export const DefaultPagination = () => {
 	);
 };
 
+export const TableDataPagination = () => {
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const onPageChange = (page: number) => setCurrentPage(page);
+
+	return (
+		<div className="flex overflow-x-auto sm:justify-center">
+			<Pagination
+				layout="table"
+				currentPage={currentPage}
+				totalPages={100}
+				onPageChange={onPageChange}
+			/>
+		</div>
+	);
+};
+
+export const NavigationPagination = () => {
+	const [currentPage, setCurrentPage] = useState(1);	
+	const onPageChange = (page: number) => setCurrentPage(page);
+
+	return (
+		<div className="flex overflow-x-auto sm:justify-center">
+			<Pagination
+				layout="pagination"
+				currentPage={currentPage}
+				totalPages={0}
+				onPageChange={onPageChange}
+			/>
+		</div>
+	);
+};
+
+export const PreviousAndNext = () => {
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const onPageChange = (page: number) => setCurrentPage(page);
+
+	return (
+		<div className="flex overflow-x-auto sm:justify-center">
+			<Pagination
+				layout="navigation"
+				currentPage={currentPage}
+				totalPages={100}
+				onPageChange={onPageChange}
+			/>
+		</div>
+	);
+};
 export const PaginationWithIcons = () => {
 	const [currentPage, setCurrentPage] = useState(1);
 
